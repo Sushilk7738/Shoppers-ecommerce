76a91d999b3fd80daaeabd480b6d7aa4cee29ef7
Sushilk <sushilkamble7738@gmail.com>
2025-12-14 16:20:20 +0530

    use stable django secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,7 +6,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # basic security
-SECRET_KEY = config("SECRET_KEY", default="unsafe-secret")
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = [
