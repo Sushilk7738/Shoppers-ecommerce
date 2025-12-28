940284ed7b621e00b20ba29e9b9c23ba1414e4f3
Sushilk <sushilkamble7738@gmail.com>
2025-12-28 12:47:29 +0530

    fix: guard order email signal to prevent worker crash

--- backend/api/signals.py
+++ backend/api/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.conf import settings
 
 from api.models import Order
 from api.view.invoice_views import generate_invoice_pdf_bytes
@@ -10,7 +11,7 @@ from api.utils.email_utils import send_order_success_email
 def order_success_email(sender, instance, created, **kwargs):
     print("SIGNAL HIT", created, instance.isPaid)
 
-    if created and instance.isPaid:
+    if created and instance.isPaid  and getattr(settings, "ENABLE_ORDER_EMAILS", False):
         try:
             pdf_content = generate_invoice_pdf_bytes(instance, instance.user)
 
