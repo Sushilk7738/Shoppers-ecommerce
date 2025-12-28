ab6c43fd5f9617785f9617674ce590f9f3c16f5b
Sushilk <sushilkamble7738@gmail.com>
2025-12-28 19:58:21 +0530

    debug: force email send when order is paid

--- backend/api/signals.py
+++ backend/api/signals.py
@@ -11,8 +11,9 @@ from api.utils.email_utils import send_order_success_email
 def order_success_email(sender, instance, created, **kwargs):
     print("SIGNAL HIT", created, instance.isPaid)
 
-    if instance.isPaid and getattr(settings, "ENABLE_ORDER_EMAILS", False):
+    if instance.isPaid:
         try:
+            print("EMAIL FUNC CALLED")
             pdf_content = generate_invoice_pdf_bytes(instance, instance.user)
 
             send_order_success_email(
