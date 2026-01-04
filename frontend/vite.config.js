581f4a916f8044995de0ca15e1cd9772a125521e
Sushilk <sushilkamble7738@gmail.com>
2026-01-04 10:26:51 +0530

    chore: add cache-busting build config for vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  publicDir:'public',
+  plugins: [react(), tailwindcss()],
+  publicDir: 'public',
+
+  // for cache safe
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+  },
 })
